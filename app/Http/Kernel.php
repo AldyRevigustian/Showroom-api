b19dd6a0e53e3846d47e7f1ae78a9016cc3006a8
AldyRevigustian <aldyrevig@gmail.com>
2023-02-11 02:54:01 +0700

    Delete Session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,6 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // 'throttle:api',
-            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
